c29ab242304c2c97ec1509813900fa4d0ae5f758
Nathan Bittner <njbittner@gmail.com>
2018-04-28 01:49:02 -0500

    landmarks now show up

--- scripts/sim.py
+++ scripts/sim.py
@@ -4,6 +4,7 @@ Simulator for running SLAM -- simulate a simple 2d environment
 """
 from numpy import pi, array, sin, cos, eye, zeros, matrix, sqrt
 from scipy.stats import chi2
+from numpy.linalg import norm
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -50,40 +51,62 @@ map_pts = np.random.rand(2, n_map_pts)
 for i in range(n_map_pts):
     map_pts[:, i] = map_lb + np.multiply(map_pts[:, i], map_ub - map_lb)
 
-seen_pt = zeros(shape=(1, n_map_pts))
+seen_pt = zeros(n_map_pts)
 
-#fig = plt.figure()
-#ax = fig.add_axes([map_lb[0], map_lb[1], map_ub[0], map_ub[1]])
-# ax.axis('equal')
+
+## INITIALIZE PLOTTING SHIT
 ax = plt.gca()
 plt.axis((-8, 8, -3, 8))
+# robot pose
 plot, = ax.plot(x_true[0], x_true[1])
-print(x_true[0:-1, 0])
+# estimation ellipse
 elipse = get_covariance_ellipse(x_true[0:-1, 0].reshape(-1,1), Sig_n, 0.9)
-print(elipse)
 plot2, = ax.plot(elipse[0,:], elipse[1,:], 'r')
+# landmarks
+plot3 = ax.scatter(map_pts[0,:], map_pts[1,:])
+print(plot3)
 
-X_true = zeros(shape=(3,n_steps))
 cov_pts_x = []
 trajectory = x_true
 
 for step in range(n_steps):
-    # propagate true state
+    ## PROPAGATE TRUE STATE
     u_true = nominal_u + np.random.multivariate_normal([0,0], Sig_n).reshape(-1,1)
     v, omega = u_true
     x_true_new = array([x_true[0]+v*dt*cos(x_true[2]), x_true[1]+v*dt*sin(x_true[2]), x_true[2] + dt*omega])
     x_true = x_true_new
     trajectory = np.concatenate((trajectory, x_true), axis=1)
-    # print(trajectory.shape)
-    # print('x_true', x_true)
+
+    ## PROPAGATE STATE ESTIMATE
+    pass
+
+    ## SENSOR UPDATE
+    all_msmts = []
+    if step % update_rate ==0:
+        # OBSERVE
+        for i in range(n_map_pts):
+            if norm(map_pts[:, i] - x_true[0:1, 0]) < max_msmt_dist:
+                seen_pt[i] = 1;
+                msmt = None
+                all_msmts.append(msmt) # append the measurement
+        # EKF update
+        pass #TODO
+
+    ## PLOT SHIT
+    #> plot true trajectory
     plot.set_data(trajectory[0,:], trajectory[1,:])
 
+    #> plot landmarks
+    #plot3.set_data(map_pts[0,:], map_pts[1,:])
+
+
+
+    #> plot estimate ellipse
     elipse = get_covariance_ellipse(x_true[0:-1, 0].reshape(-1,1), Sig_n, 0.9)
     plot2.set_data(elipse[0,:], elipse[1,:])
 
     plt.draw()
     plt.pause(0.1)
-    # X_true[:, step] = x_true
 
 plt.plot(trajectory[0,:], trajectory[1,:])
 plt.show()
